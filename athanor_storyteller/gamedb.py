14704f2d4c4b5d2ec26d0ba48d040906a4af35f3
Andrew Bastien <fubarp@gmail.com>
2020-02-07 16:56:53 -0500

    Hashing out more SToryteller stuff.

--- athanor_storyteller/gamedb.py
+++ athanor_storyteller/gamedb.py
@@ -1,8 +1,10 @@
+import re
 from evennia.typeclasses.models import TypeclassBase
-from athanor_storyteller.models import PersonaDB, TraitDefinitionDB, TraitDB
+from athanor.gamedb.base import HasAttributeGetCreate, lazy_property
+from athanor_storyteller.models import PersonaDB, TraitDefinitionDB, TraitDB, PoolDefinitionDB, PoolDB
 
 
-class DefaultPersona(PersonaDB, metaclass=TypeclassBase):
+class DefaultPersona(HasAttributeGetCreate, PersonaDB, metaclass=TypeclassBase):
 
     def setup_template(self):
         pass
@@ -16,12 +18,125 @@ class DefaultPersona(PersonaDB, metaclass=TypeclassBase):
         self.swap_typeclass(new_template, run_start_hooks='None')
         self.setup_template()
 
+    def set_trait_value(self, trait, context, value):
+        if not trait.db_allow_buy:
+            raise ValueError(f"{trait} is not a trait that can be ")
+        if value == 0 and not trait.db_allow_zero:
+            raise ValueError(f"{trait} does not allow buying at Zero!")
+        if not context:
+            context = ''
+        found_trait = None
+        if not (found_trait := self.traits.filter(db_trait_definition=trait, db_icontext=context.lower())):
+            found_trait = trait.trait_typeclass.create(persona=self, trait_def=trait, context=context, value=value)
+        else:
+            if context:
+                found_trait.db_context = context
+            found_trait.set_value(value)
+
+    def get_bonus(self, identifier):
+        """
+        Given a TraitDefinition identifier, return any bonuses this Persona is getting
+        to that trait.
+        """
+        return 0
 
 
-class DefaultTraitDefinition(TraitDefinitionDB, metaclass=TypeclassBase):
+class DefaultTraitDefinition(HasAttributeGetCreate, TraitDefinitionDB, metaclass=TypeclassBase):
+    re_normalize_0 = re.compile(r'\s+')
+    re_normalize_1 = re.compile(r"(?i)(?:^|(?<=[_\/\-\|\s()\+]))(?P<name1>[a-z]+)")
+    re_normalize_2 = re.compile(r"(?i)\b(of|the|a|and|in)\b")
+    re_normalize_3 = re.compile(r"(?i)(^|(?<=[(\|\/]))(of|the|a|and|in)")
+
+    can_mark = []
+
+    @classmethod
+    def normalize_name(cls, name):
+        name = cls.re_normalize_0.sub(' ', name.strip())
+        name = cls.re_normalize_1.sub(name.strip(), lambda find: find.group('name1').capitalize())
+        name = cls.re_normalize_2.sub(lambda find: find.group(1).lower(), name)
+        name = cls.re_normalize_3.sub(lambda find: find.group(1) + find.group(2).capitalize(), name)
+        return name
+
+class DefaultTrait(HasAttributeGetCreate, TraitDB, metaclass=TypeclassBase):
+
+    @lazy_property
+    def marked(self):
+        return self.get_or_create_attribute(key='marked', default=set())
+
+    def is_marked(self, mark):
+        return mark in self.marked
+
+    def set_marked(self, mark):
+        if self.is_marked(mark):
+            raise ValueError(f"{self} is already marked {mark}!")
+        self.marked.add(mark)
+
+    def unset_marked(self, mark):
+        if not self.is_marked(mark):
+            raise ValueError(f"{self} is already marked {mark}!")
+        self.marked.remove(mark)
+
+    @classmethod
+    def create(cls, persona, trait_def, context, value):
+        return cls(db_persona=persona, db_trait_definition=trait_def, db_context=context, db_icontext=context.lower(), db_base_value=value)
+
+    def __str__(self):
+        if self.db_context:
+            return f"{self.db_trait_definition}: {self.db_context}"
+        return str(self.db_trait_definition.db_key)
+
+    def __int__(self):
+        return int(self.db_base_value)
+
+    def _set_value(self, value):
+        """
+        Implements the meat of setting a value. Cuts down on super() usage.
+        """
+        old_value = int(self.db_value)
+        self.db_base_value = value
+        self.at_set_value(old_value)
+        return value
+
+    def at_set_value(self, old_value):
+        """
+        General abstract hook used for things like re-calculating other values.
+        """
+        pass
+
+    def at_delete(self):
+        pass
+
+    def delete_value(self, value):
+        self.at_delete()
+        self.delete()
+
+    def set_value(self, value):
+        """
+        Called by the controller to set this Trait's Value.
+        """
+        if self.db_trait_definition.db_allow_zero:
+            if value > -1:
+                return self._set_value(value)
+            else:
+                return self.delete_value(value)
+        else:
+            if value > 0:
+                return self._set_value(value)
+            else:
+                return self.delete_value(value)
+
+    def calculate(self, bonus=False):
+        """
+        Returns this trait's value for dice rolls and other purposes.
+        """
+        if bonus and (identifier := self.db_trait_definition.db_system_identifier):
+            return int(self) + self.db_persona.get_bonus(identifier)
+        return int(self)
+
+
+class DefaultPoolDefinition(HasAttributeGetCreate, PoolDefinitionDB, metaclass=TypeclassBase):
     pass
 
 
-class DefaultTrait(TraitDB, metaclass=TypeclassBase):
+class DefaultPool(HasAttributeGetCreate, PoolDB, metaclass=TypeclassBase):
     pass
-
--- athanor_storyteller/models.py
+++ athanor_storyteller/models.py
@@ -1,5 +1,10 @@
+from evennia.utils.utils import class_from_module
 from django.db import models
-from evennia.typeclasses.models import TypedObject
+from evennia.typeclasses.models import TypedObject, SharedMemoryModel
+
+
+class StorySystem(SharedMemoryModel):
+    db_key = models.CharField(max_length=255, null=False, blank=False, unique=True)
 
 
 class PersonaDB(TypedObject):
@@ -14,11 +19,17 @@ class PersonaDB(TypedObject):
     __applabel__ = "athanor_storyteller"
 
     # the db_key is the Persona's 'name.'
+    db_ikey = models.CharField(max_length=255, null=False, blank=False)
     db_object = models.ForeignKey('objects.ObjectDB', related_name='personas', on_delete=models.PROTECT)
 
     # The template key holds a string key that's used to relate the Persona to a Template
     # (Vampire, Mage, Solar, Lunar, etc). This will be used to locate a Class.
     db_template_key = models.CharField(max_length=255, default='mortal', null=False)
+
+    db_system = models.ForeignKey(StorySystem, related_name='personas', on_delete=models.PROTECT)
+
+    class Meta:
+        unique_together = (('db_object', 'db_system', 'db_ikey'))
 
 
 class TraitDefinitionDB(TypedObject):
@@ -29,12 +40,14 @@ class TraitDefinitionDB(TypedObject):
     __defaultclasspath__ = "athanor_storyteller.traits.DefaultTraitDefinition"
     __applabel__ = "athanor_storyteller"
 
+    db_system = models.ForeignKey(StorySystem, related_name='trait_definitions', on_delete=models.PROTECT)
     # db_key is used as a sort of per-parent unique identifier for when traversing the tree.
     db_parent = models.ForeignKey('self', related_name='children', null=True, on_delete=models.PROTECT)
 
     # The system identifier is a string used to identify this trait in the database for the plugin
     # system to get-create-update it. this is usually a flat and simple string, like attribute_strength
-    db_system_identifier = models.CharField(max_length=255, null=True, blank=False, unique=True)
+    # This identifier will also be used for bonuses and other purposes.
+    db_system_identifier = models.CharField(max_length=255, null=True, blank=False)
 
     # The child and trait typeclass fields contain python paths to which typeclasses will be used for
     # new children of this Definition, and a Trait based on this definition, respectively.
@@ -48,6 +61,21 @@ class TraitDefinitionDB(TypedObject):
     db_allow_context = models.BooleanField(default=False, null=False)
     db_require_context = models.BooleanField(default=False, null=False)
 
+    db_can_specialize = models.BooleanField(default=False, null=False)
+    db_can_roll = models.BooleanField(default=False, null=False)
+
+    # A Trait that allows Zero can be 'purchased' at 0 so you can use it as a base for other traits.
+    # This has no affect on automated add processes, only the UI.
+    db_allow_zero = models.BooleanField(default=False, null=False)
+
+    # If this is false, this trait cannot be bought. SEt this false for any TraitDefinition that
+    # only serves as a 'Category' parent.
+    db_allow_buy = models.BooleanField(default=True, null=False)
+
+    # Marks whether this trait has been approved by staff or not. This only affects the UI
+    # experience during Chargen. Relevant only for traits created during play.
+    db_approved = models.BooleanField(default=True, null=False)
+
     def __str__(self):
         return str(self.db_key)
 
@@ -59,8 +87,16 @@ class TraitDefinitionDB(TypedObject):
             par = par.db_parent
         return '/'.join(str(trait) for trait in reversed(full))
 
+    @property
+    def trait_typeclass(self):
+        if not (default := self.db_trait_default_typeclass) and not self.db_parent:
+            return self.__class__
+        if default:
+            return class_from_module(default)
+        return self.db_parent.trait_typeclass
+
     class Meta:
-        unique_together = (('db_parent', 'db_key'), ('db_parent', 'db_formal_name'),)
+        unique_together = (('db_system', 'db_parent', 'db_key'), ('db_system', 'db_system_identifier'))
         verbose_name = 'TraitDefinition'
         verbose_name_plural = 'TraitDefinitions'
 
@@ -75,7 +111,7 @@ class TraitDB(TypedObject):
 
     # db_key isn't really important here; don't rely on it for anything.
     db_persona = models.ForeignKey(PersonaDB, related_name='traits', on_delete=models.CASCADE)
-    db_trait = models.ForeignKey(TraitDefinitionDB, related_name='traits', on_delete=models.PROTECT)
+    db_trait_definition = models.ForeignKey(TraitDefinitionDB, related_name='traits', on_delete=models.PROTECT)
     db_context = models.CharField(max_length=255, null=False, blank=True, default='')
     db_icontext = models.CharField(max_length=255, null=False, blank=True, default='')
     db_base_value = models.BigIntegerField(default=0, null=False, blank=False)
@@ -85,3 +121,32 @@ class TraitDB(TypedObject):
         unique_together = (('db_persona', 'db_trait', 'db_icontext'),)
         verbose_name = 'Trait'
         verbose_name_plural = 'Traits'
+
+    def fullpath(self):
+        if self.db_context:
+            return f"{self.db_trait_definition.fullpath()}: {self.db_context}"
+        return self.db_trait_definition.fullpath()
+
+
+class PoolDefinitionDB(TypedObject):
+    __settingclasspath__ = "athanor_storyteller.traits.DefaultPoolDefinition"
+    __defaultclasspath__ = "athanor_storyteller.traits.DefaultPoolDefinition"
+    __applabel__ = "athanor_storyteller"
+
+    db_system = models.ForeignKey(StorySystem, related_name='trait_definitions', on_delete=models.PROTECT)
+    db_system_identifier = models.CharField(max_length=255, null=True, blank=False)
+
+    class Meta:
+        unique_together = (('db_system', 'db_system_identifier'), )
+        verbose_name = 'PoolDefinition'
+        verbose_name_plural = 'PoolDefinitions'
+
+
+class PoolDB(TypedObject):
+    __settingclasspath__ = "athanor_storyteller.traits.DefaultPool"
+    __defaultclasspath__ = "athanor_storyteller.traits.DefaultPool"
+    __applabel__ = "athanor_storyteller"
+
+    db_persona = models.ForeignKey(PersonaDB, related_name='pools', on_delete=models.CASCADE)
+    db_pool_definition = models.ForeignKey(PoolDefinitionDB, related_name='pools', on_delete=models.PROTECT)
+    db_bonus_maximum = models.IntegerField(default=0, null=False, blank=False)
